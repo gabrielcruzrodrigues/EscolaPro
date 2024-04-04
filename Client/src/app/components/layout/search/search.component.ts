b1799f780fec5a228121d96501bb431ba48fce2a
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-04-04 10:39:44 -0300

    create input search

--- Client/src/app/components/layout/search/search.component.ts
+++ Client/src/app/components/layout/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
+  selector: 'search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
